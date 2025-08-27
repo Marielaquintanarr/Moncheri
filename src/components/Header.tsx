209d2851f1882d4260ec00579f97202f2b6326dc
Marielaquintanar <marielaqntrr@gmail.com>
2025-08-26 18:22:29 -0600

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+
+export function Header() {
+    return (
+      <>
+      <div style={{}}>
+        <div style={{display: "flex", gap: "200px", fontSize: "32px", fontWeight: '100'}}>
+            <p>Nosotros</p>
+            <p>Productos</p>
+            <p>Envios</p>
+            <p>Contacto</p>
+        </div>
+        <h1 style={{fontSize: '180px', fontWeight: '100',  marginTop: "-60px", marginBottom: "0"}}>
+            moncheri joyeria
+        </h1>
+      </div>
+      </>
+    )
+}
+  
+  
